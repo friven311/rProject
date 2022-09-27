9bb0b3fb78396f23c32732609e3882c2f11f64de
Viktor Shalamov <Viktor.Shalamov@effective-soft.com>
2022-09-27 12:31:39 +0300

    added airbnb deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    ['airbnb', 'airbnb/hooks'],
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
