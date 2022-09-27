93759301ec0b91da4e88eb255dc35235fd5394a8
Viktor Shalamov <Viktor.Shalamov@effective-soft.com>
2022-09-27 12:14:16 +0300

    added react-native-global

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,37 @@
 module.exports = {
-    'env': {
-        'es2021': true,
-        'node': true,
-        
+  env: {
+    es2021: true,
+    node: true,
+    'react-native-globals/all': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      tsx: true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended'
-    ],
-    'overrides': [
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module',
-        'ecmaFeatures': {
-            'tsx': true
-         } 
-    },
-    'plugins': [
-        'react',
-        '@typescript-eslint',
-        'react-native',
-        
-    ],
-    'rules': {
-        'linebreak-style': [ 'error', 'unix' ],
-        'quotes': [ 'error', 'single' ],
-        'semi': [ 'error', 'always' ],
-        'react-native/no-unused-styles': 2,
-        'react-native/split-platform-components': 2,
-        'react-native/no-inline-styles': 2,
-        'react-native/no-color-literals': 2,
-        'react-native/no-raw-text': 2,
-    }
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'react-native',
+    'react-native-globals',
+  ],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+  },
 };
