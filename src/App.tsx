9e8d05f54eae47425ccc976da36bea142d307d34
Viktor Shalamov <Viktor.Shalamov@effective-soft.com>
2022-10-03 16:22:57 +0300

    fix: implemented 1 button with props for each case

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import {View, useColorScheme} from 'react-native';
+
 import {ThemeProvider} from 'styled-components';
-import ButtonBlack from './components/Buttons/ButtonBlack';
+import Button from './components/Buttons/Button';
 import {lightTheme, darkTheme} from './theme';
-import ButtonGrey from './components/Buttons/ButtonGrey';
-import ButtonWhite from './components/Buttons/ButtonWhite';
 import ButtonServiceAuth from './components/Buttons/ButtonServiceAuth';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const onPress = () => {
+    console.log('buttonPressed');
+  };
   return (
     <View>
       <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-        <ButtonBlack title="Let's get started" />
-        <ButtonGrey title="Let's get started" />
-        <ButtonWhite title="Let's get started" />
+        <Button title="Let's get started" color="black" onPress={onPress} />
+        <Button title="Let's get started" color="grey" onPress={onPress} />
+        <Button title="Let's get started" color="white" onPress={onPress} />
         <View style={{flexDirection: 'row'}}>
-          <ButtonServiceAuth flag="Apple" />
-          <ButtonServiceAuth flag="Google" />
+          <ButtonServiceAuth registrationType="Apple" />
+          <ButtonServiceAuth registrationType="Google" />
         </View>
       </ThemeProvider>
     </View>
