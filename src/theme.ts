ffd93a446fca1a3666770d76d064fc743a8c47cd
Viktor Shalamov <Viktor.Shalamov@effective-soft.com>
2022-10-03 09:33:49 +0300

    feat: added theme by styled-components

---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import {DefaultTheme} from 'styled-components';
+
+const lightTheme: DefaultTheme = {
+  colors: {
+    primaryBlack: '#000000',
+    primaryWhite: '#ffffff',
+    primaryGrey: '#3940f9',
+  },
+};
+
+const darkTheme: DefaultTheme = {
+  colors: {
+    primaryBlack: '#000000',
+    primaryWhite: '#ffffff',
+    primaryGrey: '#3940f9',
+  },
+};
+
+export {lightTheme, darkTheme};
